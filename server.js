47c98bfd0042fbd8372c441dbf20adaacf2efadb
pcancio <pcancio@gmail.com>
2022-02-19 00:53:51 -0500

    app working

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const mongoose = require('mongoose');
 const express = require('express');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Social-Network-API', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-mongoose.set('debug', true);
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
 
